f26b42f98ea8717b86dd461db85c2a6428d53964
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-05-26 18:17:43 +1000

    Format code

--- VSMS/src/vsms/DataBaseManager.java
+++ VSMS/src/vsms/DataBaseManager.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vsms;
 
 import java.sql.DriverManager;
@@ -10,8 +5,6 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import vsms.customExceptions.QueryInvalidException;
 
 /**
@@ -56,46 +49,35 @@ public class DataBaseManager {
     boolean connectionState(int timeOut) {
         return checkConnection(timeOut); //custom timeout
     }
-    
+
     //throws exception so it can be handled in main code/gui
-    void SetSqlQuery(String SQL) throws SQLException
-    {       
-            this.query = this.connection.prepareStatement(SQL);       
+    void SetSqlQuery(String SQL) throws SQLException {
+        this.query = this.connection.prepareStatement(SQL);
     }
-    
+
     //throws both Query invalid which indicates no query has been created or sql exception for anything else.
-    ResultSet executeQuery(String SQLQuery) throws QueryInvalidException, SQLException 
-    {
-        if(query == null)
-        {
+    ResultSet executeQuery(String SQLQuery) throws QueryInvalidException, SQLException {
+        if (query == null) {
             throw new QueryInvalidException("Please create a query first");
-        }else
-        {
+        } else {
             return query.executeQuery();
         }
     }
-    
-    void setQueryString(int index, String value) throws SQLException
-    {
+
+    void setQueryString(int index, String value) throws SQLException {
         query.setString(index, value);
     }
-    
-    void setQueryInt(int index, int value) throws SQLException
-    {
+
+    void setQueryInt(int index, int value) throws SQLException {
         query.setInt(index, value);
     }
-    
-    
-    
-    
+
     //This will be a fun one
-    void setQueryDate(int index, java.util.Date date) throws SQLException
-    {
+    void setQueryDate(int index, java.util.Date date) throws SQLException {
         this.query.setDate(index, DateTools.JavaDateToSqlDate(date));
     }
-    
-     void setQueryDate(int index, java.sql.Date date) throws SQLException
-    {
+
+    void setQueryDate(int index, java.sql.Date date) throws SQLException {
         query.setDate(index, date);
     }
 
--- VSMS/src/vsms/DateTools.java
+++ VSMS/src/vsms/DateTools.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vsms;
-
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
 
 /**
  *
@@ -18,34 +11,31 @@ public class DateTools {
 
     public static java.sql.Date StringToDate(String str, String format) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
-        
-            java.util.Date d1 = sdf.parse(str);
-            sdf.applyPattern("yyyy-mm-dd");
-            String tmp = sdf.format(d1); //yyyy-[m]m-[d]d format to this format because mysql date class is stupid and only understands american freedom formats
-            System.out.println("In StringToDate: " + tmp);
-            return java.sql.Date.valueOf(tmp);
-                
-        
-        
+
+        java.util.Date d1 = sdf.parse(str);
+        sdf.applyPattern("yyyy-mm-dd");
+        String tmp = sdf.format(d1); //yyyy-[m]m-[d]d format to this format because mysql date class is stupid and only understands american freedom formats
+        System.out.println("In StringToDate: " + tmp);
+        return java.sql.Date.valueOf(tmp);
+
     }
-    
-    public static java.sql.Date JavaDateToSqlDate(java.util.Date javaDate)
-    {
+
+    public static java.sql.Date JavaDateToSqlDate(java.util.Date javaDate) {
         return new java.sql.Date(javaDate.getTime());
     }
-    
+
     public static String SqlDateToString(java.util.Date date, String format) throws ParseException {
-        
-            SimpleDateFormat sdf = new SimpleDateFormat(format);
 
-            String dateStr = date.toString();
-            System.out.println(dateStr);
-            java.util.Date d1 = sdf.parse(dateStr);
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
 
-            sdf.applyPattern("dd/mm/yyyy");
-            String tmp = sdf.format(d1);
-            System.out.println(d1.toString());
-            return tmp;
-        
+        String dateStr = date.toString();
+        System.out.println(dateStr);
+        java.util.Date d1 = sdf.parse(dateStr);
+
+        sdf.applyPattern("dd/mm/yyyy");
+        String tmp = sdf.format(d1);
+        System.out.println(d1.toString());
+        return tmp;
+
     }
 }
