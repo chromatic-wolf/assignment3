bae7359ab07235bad25854f5b7f5d68282726602
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-05-30 17:37:40 +1000

    Added connection code

--- VSMS/src/vsms/DataBaseLoginController.java
+++ VSMS/src/vsms/DataBaseLoginController.java
@@ -7,10 +7,12 @@ package vsms;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -24,11 +26,26 @@ public class DataBaseLoginController implements Initializable {
     Button ui_connect_btn;
     TextField ui_address_txt;
     TextField ui_password_txt;
-    
+
     //This runs when the window first opens so putting any event listeners here
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
-    
+        //connect button listener
+        ui_connect_btn.setOnAction((ActionEvent e) -> {
+            //code to run goes here
+            //Creates a database connection object
+            DataBaseManager database = new DataBaseManager();
+            //Attempts to connect to the database, using default username for now maybe add ui component to change username later.
+            if (database.connect(ui_address_txt.getText(), "root", ui_password_txt.getText(), "purchase")) {
+                //Database connect
+                //Open next window
+            } else {
+                //failed to connect
+                //display error message
+                JOptionPane.showMessageDialog(null, "Error cannot connect", "InfoBox: " + "Error", JOptionPane.ERROR_MESSAGE);
+
+            }
+        });
+    }
+
 }
