d5e5cb5d337c9888545cedf62deaa729c9181757
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-05-31 12:43:12 +1000

    Added template code
    Added blank listeners and format code

--- VSMS/src/vsms/MainWindowController.java
+++ VSMS/src/vsms/MainWindowController.java
@@ -2,6 +2,7 @@ package vsms;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,13 +20,22 @@ public class MainWindowController implements Initializable {
     Button ui_vehicle_management_btn;
     @FXML
     Button ui_service_management_btn;
+
     @FXML
 
-
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        ui_customer_management_btn.setOnAction((ActionEvent e) -> {
+            //open customer management window
+        });
+        ui_vehicle_management_btn.setOnAction((ActionEvent e) -> {
+            //open vehicle management window
+        });
+        ui_service_management_btn.setOnAction((ActionEvent e) -> {
+            //open service management window
+        });
+
+    }
+
 }
