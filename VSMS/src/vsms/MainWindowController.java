8f9b0f49aa860e4cfb2a919dceaaeda29fb89091
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-06-07 19:05:38 +1000

    Removed old handlers

--- VSMS/src/vsms/MainWindowController.java
+++ VSMS/src/vsms/MainWindowController.java
@@ -33,17 +33,7 @@ public class MainWindowController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //There are 3 main windows which will allow you to basically view the contents of a table in the database.
-        // There will be more windows that will allow you to view data linked to other data for example you can click on a customer and view their vehicles or services.
-        ui_customer_management_btn.setOnAction((ActionEvent e) -> {
-            //open customer management window
-        });
-        ui_vehicle_management_btn.setOnAction((ActionEvent e) -> {
-            //open vehicle management window
-        });
-        ui_service_management_btn.setOnAction((ActionEvent e) -> {
-            //open service management window
-        });
+        
 
     }
 
