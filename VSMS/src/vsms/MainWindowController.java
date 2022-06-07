6e07ff4280c2d1877ae8c7584bf7cf8d6efe435d
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-06-07 20:38:45 +1000

    UI and added script (not finished)

--- VSMS/src/vsms/MainWindowController.java
+++ VSMS/src/vsms/MainWindowController.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 /**
@@ -33,9 +34,11 @@ public class MainWindowController implements Initializable {
     Button ui_search_btn;
     @FXML
     Button ui_add_cust_btn;
-    Connection database;
-        ObservableList<Customer> list = FXCollections.observableArrayList();
+    @FXML
+    TableView ui_table;
 
+    Connection database;
+    ObservableList<Customer> list = FXCollections.observableArrayList();
 
     public void initData(DataBaseManager database) {
         this.database = database.getConnectionObject();
@@ -45,43 +48,41 @@ public class MainWindowController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         ui_search_btn.setOnAction((ActionEvent e) -> {
             //Call search function/ search logic here
-           
-             String sql = "SELECT * FROM purchase.customers WHERE CUSTOMERID LIKE NULL OR FIRSTNAME LIKE ? AND LASTNAME LIKE ? AND PHONE LIKE ?;";
-        try {
-            PreparedStatement searchCustomer = database.prepareStatement(sql);
 
-            searchCustomer.setString(1, ui_first_name_field.getText() + '%');
-            searchCustomer.setString(2, ui_last_name_field.getText() + '%');
-            searchCustomer.setString(3, ui_address_field.getText() + '%');
-            searchCustomer.setString(4, ui_phone_field.getText() + '%');
+            String sql = "SELECT * FROM purchase.customers WHERE CUSTOMERID LIKE NULL OR FIRSTNAME LIKE ? AND LASTNAME LIKE ? AND ADDRESS LIKE ? AND PHONE LIKE ?;";
+            try {
+                PreparedStatement searchCustomer = database.prepareStatement(sql);
 
+                searchCustomer.setString(1, ui_first_name_field.getText() + '%');
+                searchCustomer.setString(2, ui_last_name_field.getText() + '%');
+                searchCustomer.setString(3, ui_address_field.getText() + '%');
+                searchCustomer.setString(4, ui_phone_field.getText() + '%');
 
-            ResultSet rs = searchCustomer.executeQuery();
-            int index = 1;
-            list.clear();
-            while (rs.next()) {
-                /*
+                ResultSet rs = searchCustomer.executeQuery();
+                int index = 1;
+                list.clear();
+                while (rs.next()) {
+                    /*
                 if (rs.getString(1).compareToIgnoreCase("Test") == 0) {
                    
                 }
-                 */
-                list.add(new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
+                     */
+                    list.add(new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
 
-                index++;
+                    index++;
 
+                }
+                for (int i = 0; i < list.size(); i++) {
+                    list.get(i).printAll();
+                }
+                //ui_table.setItems(list);
+
+            } catch (SQLException ex) {
+                System.out.println(ex);
             }
-            for (int i = 0; i < list.size(); i++) {
-                list.get(i).printAll();
-            }
-            //ui_table.setItems(list);
 
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-            
-            
         });
-        
+
         ui_add_cust_btn.setOnAction((ActionEvent e) -> {
             //Call insert function/ insert logic here
         });
