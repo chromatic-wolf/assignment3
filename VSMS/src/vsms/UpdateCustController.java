2c9dff52d4bb73f82ca996e018fcbc1404651568
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-06-10 21:54:22 +1000

    Binding and close button

--- VSMS/src/vsms/UpdateCustController.java
+++ VSMS/src/vsms/UpdateCustController.java
@@ -7,21 +7,49 @@ package vsms;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author caleb
  */
+
+ 
+
 public class UpdateCustController implements Initializable {
 
+    @FXML
+    TextField ui_first_name_field;
+    @FXML
+    TextField ui_last_name_field;
+    @FXML
+    TextField ui_address_field;
+    @FXML
+    TextField ui_phone_field;
+    @FXML
+    Button ui_update_btn;
+    @FXML
+    Button ui_cancel_btn;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        ui_cancel_btn.setOnAction((ActionEvent e) -> {
+                Stage stage = (Stage) ui_cancel_btn.getScene().getWindow();
+                stage.close();
+        });
+        
+        ui_update_btn.setOnAction((ActionEvent e) -> {
+            
+        });
     }    
     
 }
